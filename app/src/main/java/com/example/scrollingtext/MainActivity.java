9e672b662604dac78d678afba7a13e89ad55c920
CrisM93 <criztian.gio@gmail.com>
2022-12-15 20:42:37 -0600

    Otra tarea con scrolltolbar y texto plano

--- app/src/main/java/com/example/scrollingtext/MainActivity.java
+++ app/src/main/java/com/example/scrollingtext/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.scrollingtext;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+//Esta es otra tarea de texto en donde se muestran scroll y texto plano.
 public class MainActivity extends AppCompatActivity {
 
     @Override
